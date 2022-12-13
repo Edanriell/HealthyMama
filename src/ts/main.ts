dfe18e3a0359efcab3695c33a35e4d40cbf054ef
Lauris <edanriell@gmail.com>
2022-12-13 21:03:49 +0200

    added modernizr for webp images

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,13 @@
 import "../scss/style.scss";
+import "../js/vendors/modernizr.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+	console.log("works")
+	Modernizr.on("webp", result => {
+		if (result) {
+			console.log(result);
+		} else {
+			console.log(result);
+		}
+	});
+});
