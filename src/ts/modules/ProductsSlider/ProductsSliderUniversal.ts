454db345296fba601370a58dec825842960921bf
Lauris <edanriell@gmail.com>
2023-02-20 20:15:15 +0200

    added products slider

--- src/ts/modules/ProductsSlider/ProductsSliderUniversal.ts
+++ src/ts/modules/ProductsSlider/ProductsSliderUniversal.ts
@@ -3,11 +3,27 @@ import { SwiperOptions } from "swiper";
 export const ProductsSliderUniversal: SwiperOptions = {
 	slidesPerView: "auto",
 	loop: true,
-	// centeredSlides: true,
+	autoplay: {
+		delay: 8000,
+		disableOnInteraction: true,
+		pauseOnMouseEnter: true
+	},
+	keyboard: {
+		enabled: true
+	},
+	mousewheel: false,
 	grabCursor: true,
 	centeredSlides: true,
-	spaceBetween: 18
-	// centeredSlidesBounds: true
+	spaceBetween: 18,
+	speed: 500,
+	navigation: {
+		nextEl: ".products-slider__next-slide-button",
+		prevEl: ".products-slider__previous-slide-button"
+	},
+	breakpoints: {
+		1024: {
+			spaceBetween: 17,
+			mousewheel: true
+		}
+	}
 };
-
-// Fix main-slider-universal padding !!!
